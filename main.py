9db304f27ba510d13da8fe201eb276365e05b7bc
Luiz Orlando <128615584+LuizOrla@users.noreply.github.com>
2026-07-24 17:58:23 -0300

    Update main.py
    Atualização do arquivo main

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 # Arquivo principal de orquestração
 
-import classes
-import storage
-import gui
-import config
+from classes import Projeto
+from classes import Tarefas
+from storage import coletar_projetos
+from storage import coletar_tarefas
+from storage import atualizar_projetos
+from storage import criar_projeto
+from storage import apagar_projeto
+
+from gui import cabecalho
+from gui import menu_lateral
+from gui import corpo
+
+from config import ARQUIVO_STORAGE
 
 from nicegui import ui
+
+
+lista_proj = coletar_projetos()
+# lista_task = coletar_tarefas()
+
+cabecalho("Projeto Teste")
+menu_lateral(lista_proj)
+corpo()
+ui.run()
+
 
 # Iniciar Aplicação
 #   Cabeçalho

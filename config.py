952ad89d9d59536b0868e27b6995cf4e3cacfe02
Luiz Orlando <128615584+LuizOrla@users.noreply.github.com>
2026-07-28 13:27:53 -0300

    Update config.py
    Criação de arquivo de tarefas, e dicionários globais.

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
-# Arquivo de constantes da aplicação
-ARQUIVO_STORAGE = "data/projects.json"
+# Arquivo de constantes e configurações da aplicação
+ARQUIVO_PROJETO = "data/projects.json"
+ARQUIVO_TAREFAS = "data/tarefas.json"
 
+dict_projetos = {}
+dict_tarefas = {}

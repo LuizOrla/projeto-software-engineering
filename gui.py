8b4ba572b02a3622eac08baca2640ee4d428f231
Luiz Orlando <128615584+LuizOrla@users.noreply.github.com>
2026-07-28 13:27:22 -0300

    Update gui.py
    Atualização: menu_lateral() e corpo() atualizáveis (refreshable).

--- gui.py
+++ gui.py
@@ -1,60 +1,53 @@
 # Interface GUI da aplicação
 from nicegui import ui
-from storage import coletar_projetos
-from storage import criar_tarefa
-
-# ========== Objetos ==========
-# Cabeçalho
-# Menu Lateral
-#   Botão Criar Tarefa
-#   Botão Salvar Projeto
-#   Botão Carregar Projeto
-# Nome do Projeto
-# Descrição
-# Colunas
-#   Cartões de Tarefas
 
 def cabecalho(titulo):
     with ui.header().classes("bg-blue items-center justify-center text-white py-6"):
         ui.label(titulo).classes("text-3xl font-bold absolute-center")
-
-def menu_lateral(lista_projeto):
-    lista = coletar_projetos()
+@ui.refreshable
+def menu_lateral(dict_projetos):
     with ui.left_drawer(value=True).classes("bg-slate-100 p-4") as menu_lateral:
         ui.label("Menu").classes("text-lg mb-1")
         ui.button("Nova Tarefa", icon="add", on_click=janela_nova_tarefa).props("flat").classes("w-full justify-start")
-        ui.button("Salvar Projeto", icon="save", on_click=lambda: ui.notify("Salvar Projeto")).props("flat").classes("w-full justify-start")
+        ui.button("Salvar Projeto", icon="save", on_click=janela_novo_projeto).props("flat").classes("w-full justify-start")
         ui.separator()
         ui.label("Projetos Salvos").classes("text-lg mb-1")
-        for projeto in lista_projeto:
+        for id, projeto in dict_projetos:
             with ui.item().classes("w-full p-2 gap-0.5 justify-between"):
                 with ui.item_section():
-                    ui.item_label(projeto[1]).classes("text-base font-semibold")
-                    ui.item_label(projeto[0]).classes("text-xs text-gray-400")
+                    ui.item_label(projeto["titulo"]).classes("text-base font-semibold")
+                    ui.item_label(id).classes("text-xs text-gray-400")
                 ui.button(icon="open_in_new", on_click=lambda: ui.notify("Abrir Projeto")).props("flat dense")
                 ui.button(icon="delete", on_click=lambda: ui.notify("Apagar Projeto")).props("flat dense")
+@ui.refreshable
+def corpo(dict_projeto, id_projeto=""):
+    if id_projeto == "" or id_projeto not in dict_projeto:
+        label_projeto = "Projeto"
+        label_descricao = "Descrição"
+    else:
+        projeto = dict_projeto[id_projeto]
+        label_projeto = projeto["titulo"]
+        label_descricao = projeto["descricao"]
 
-def corpo(lista_tarefas=[]):
     with ui.element("div").classes("items-center justify-center"):
-        ui.label("Projeto").classes("text-lg font-semibold mb-1 text-center")
-        ui.label("Descrição").classes("text-center")
+        ui.label(label_projeto).classes("text-lg font-semibold mb-1 text-center")
+        ui.label(label_descricao).classes("text-center")
     with ui.row().classes("w-full justify-center "):
         with ui.card().classes("bg-gray-100 p-4 rounded-lg shadow-none border border-gray-200 w-2/8 h-[700px] overflow-y-auto items-start"):
             ui.label("A Fazer").classes("text-lg font-semibold")
-            for tarefa in lista_tarefas:
-                if tarefa[3] == "todo":
-                    card_tarefa(tarefa[0], tarefa[1], tarefa[2], tarefa[3])
+            for id_tarefa, tarefa in dict_projeto[id_projeto]:
+                if tarefa["satus"] == "todo":
+                    card_tarefa(id_tarefa, tarefa["nome"], tarefa["descricao"], tarefa["status"])
         with ui.card().classes("bg-gray-100 p-4 rounded-lg shadow-none border border-gray-200 w-2/8 h-[700px] overflow-y-auto items-start"):
             ui.label("Em Andamento").classes("text-lg font-semibold")
-            for tarefa in lista_tarefas:
-                if tarefa[3] == "doing":
-                    card_tarefa(tarefa[0], tarefa[1], tarefa[2], tarefa[3])
+            for id_tarefa, tarefa in dict_projeto[id_projeto]:
+                if tarefa["satus"] == "doing":
+                    card_tarefa(id_tarefa, tarefa["nome"], tarefa["descricao"], tarefa["status"])
         with ui.card().classes("bg-gray-100 p-4 rounded-lg shadow-none border border-gray-200 w-2/8 h-[700px] overflow-y-auto items-start"):
             ui.label("Concluído").classes("text-lg font-semibold")
-            for tarefa in lista_tarefas:
-                if tarefa[3] == "done":
-                    card_tarefa(tarefa[0], tarefa[1], tarefa[2], tarefa[3])
-
+            for id_tarefa, tarefa in dict_projeto[id_projeto]:
+                if tarefa["satus"] == "done":
+                    card_tarefa(id_tarefa, tarefa["nome"], tarefa["descricao"], tarefa["status"])
 def card_tarefa(id, nome, descricao, status):
     with ui.card().classes("w-full p-2 gap-0.5 justify-between"):
         ui.label(nome).classes("text-base font-semibold")
@@ -72,12 +65,22 @@ def janela_nova_tarefa():
     with ui.dialog() as dialogo, ui.card().classes("w-2/5"):
         ui.label("Nova Tarefa")
         nome_tarefa = ui.input(label="Nome da Tarefa").classes("w-full")
+        tarefa = nome_tarefa.value()
         desc_tarefa = ui.input(label="Descrição").classes("w-full")
+        descricao = desc_tarefa.value()
         with ui.row().classes("w-full justify-end"):
             ui.button("Cancelar", on_click=dialogo.close).props("flat")
-            ui.button("Criar Tarefa", on_click=lambda: tarefa_nova(nome_tarefa.value, desc_tarefa.value))
+            ui.button("Criar Tarefa", on_click=lambda: ui.notify("Nova Tarefa"))
+    dialogo.open()
+def janela_novo_projeto():
+    with ui.dialog() as dialogo, ui.card().classes("w-2/5"):
+        ui.label("Salvar Projeto")
+        nome_projeto = ui.input(label="Título do Projeto"). classes("w-full")
+        projeto = nome_projeto.value()
+        desc_projeto = ui.input(label="Descrição").classes("w-full")
+        descricao = desc_projeto.value()
+        with ui.row().classes("w-full justify-end"):
+            ui.button("Cancelar", on_click=dialogo.close).props("flat")
+            ui.button("Salvar Projeto", on_click=lambda: ui.notify("Salvar Projeto"))
     dialogo.open()
 
-def tarefa_nova(nome, desc):
-    lista = criar_tarefa(nome, desc)
-    card_tarefa(lista[0],lista[1], lista[2], lista[3])

944b94b9db78bf35debed60e825a89431e8c7979
Luiz Orlando <128615584+LuizOrla@users.noreply.github.com>
2026-07-30 18:00:23 -0300

    Update config.py
    Atualização de caminho para arquivos, dicionários e projetos ativos.

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 # Arquivo de constantes e configurações da aplicação
-ARQUIVO_PROJETO = "data/projects.json"
-ARQUIVO_TAREFAS = "data/tarefas.json"
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+ARQUIVO_PROJETO = os.path.join(BASE_DIR, "data", "projetos.json")
+ARQUIVO_TAREFAS = os.path.join(BASE_DIR, "data", "tarefas.json")
 
 dict_projetos = {}
 dict_tarefas = {}
+
+projeto_ativo = ""

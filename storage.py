628ec21edc06627e87119b7f061fb6e2336652fc
Luiz Orlando <128615584+LuizOrla@users.noreply.github.com>
2026-07-30 17:59:48 -0300

    Update storage.py
    Atualização de funções: mover tarefa, carregar projeto.

--- storage.py
+++ storage.py
@@ -2,24 +2,35 @@
 from classes import Projeto
 from classes import Tarefa
 import config
-from config import ARQUIVO_PROJETO
-from config import ARQUIVO_TAREFAS
 import json
+from json import JSONDecodeError
 import uuid
 
 # ========== Funções de Arquivos ==========
 def ler_arquivos():
-    with open(ARQUIVO_PROJETO, "r", encoding="utf-8") as arquivo:
-        config.dict_projetos = json.load(arquivo)
-    with open (ARQUIVO_TAREFAS, "r", encoding="utf-8") as arquivo:
-        config.dict_tarefas = json.loads(arquivo)
-def salvar_arquivos(dict_projeto=0, dict_tarefa=0):
-    if dict_projeto is not 0:
-        with open(ARQUIVO_PROJETO, "w", encoding="utf-8") as arquivo:
-            json.dumps(dict_projeto, arquivo, indent=4, ensure_ascii=False)
-    if dict_tarefa is not 0:
-        with open(ARQUIVO_TAREFAS, "w", encoding="utf-8") as arquivo:
-            json.dumps(dict_tarefa, arquivo, indent=4, ensure_ascii=False)
+    try:
+        with open(config.ARQUIVO_PROJETO, "r", encoding="utf-8") as arquivo:
+            config.dict_projetos = json.load(arquivo)
+    except (JSONDecodeError, FileNotFoundError):
+        dict = {}
+        salvar_projetos_arquivo(dict)
+    try:
+        with open (config.ARQUIVO_TAREFAS, "r", encoding="utf-8") as arquivo:
+            config.dict_tarefas = json.load(arquivo)
+    except (JSONDecodeError, FileNotFoundError):
+        dict = {}
+        salvar_tarefas_arquivo(dict)  
+def salvar_arquivos(dict_projeto={}, dict_tarefa={}):
+    if dict_projeto != {}:
+        salvar_projetos_arquivo(dict_projeto)
+    if dict_tarefa != {}:
+        salvar_tarefas_arquivo(dict_tarefa)
+def salvar_projetos_arquivo(dict_projeto):
+    with open(config.ARQUIVO_PROJETO, "w", encoding="utf-8") as arquivo:
+        json.dump(dict_projeto, arquivo, indent=4, ensure_ascii=False)
+def salvar_tarefas_arquivo(dict_tarefa):
+    with open(config.ARQUIVO_TAREFAS, "w", encoding="utf-8") as arquivo:
+        json.dump(dict_tarefa, arquivo, indent=4, ensure_ascii=False)
 
 # ========== Funções de Projetos ==========
 def apagar_projeto(id_projeto):
@@ -28,21 +39,34 @@ def apagar_projeto(id_projeto):
         if tarefa["projeto"] == id_projeto:
             del config.dict_tarefas[id_tarefa]
     salvar_arquivos(dict_projeto=config.dict_projetos, dict_tarefa=config.dict_tarefas)
-def criar_projeto(titulo, descricao):
-    id_projeto = str(uuid.uuid4())
+def criar_projeto(titulo, descricao, id_projeto=""):
+    if id_projeto == "":
+        id_projeto = str(uuid.uuid4())
     projeto = Projeto(id_projeto, titulo, descricao)
-    config.dict_projeto[id_projeto] = projeto.dicionario_projeto()
+    id_projeto, dict_projeto = projeto.dicionario_projeto()
+    config.dict_projetos[id_projeto] = dict_projeto
     salvar_arquivos(dict_projeto=config.dict_projetos)
+    config.projeto_ativo = id_projeto
 
 # ========== Funções de Tarefas ==========
 def criar_tarefa(nome, descricao):
     id_tarefa = str(uuid.uuid4())
-    tarefa = Tarefa(id_tarefa, nome, descricao)
-    config.dict_tarefas[id_tarefa] = tarefa.dicionario_tarefa()
+    tarefa = Tarefa(id_tarefa, config.projeto_ativo, nome, descricao)
+    id_tarefa, dict_tarefa = tarefa.dicionario_tarefa()
+    config.dict_tarefas[id_tarefa] = dict_tarefa
     salvar_arquivos(dict_tarefa=config.dict_tarefas)
-def mover_tarefa(id_tarefa, novo_status):
-    with config.dict_tarefas[id_tarefa] as tarefa:
-        tarefa["status"] = novo_status
+def avancar_tarefa(id_tarefa):
+    for id_task, tarefa in config.dict_tarefas.items():
+        if id_task == id_tarefa:
+            match tarefa["status"]:
+                case "todo": tarefa["status"] = "doing"
+                case "doing": tarefa["status"] = "done"
+def retornar_tarefa(id_tarefa):
+    for id_task, tarefa in config.dict_tarefas.items():
+        if id_task == id_tarefa:
+            match tarefa["status"]:
+                case "doing": tarefa["status"] = "todo"
+                case "done": tarefa["status"] = "doing"
 def apagar_tarefa(id_tarefa):
     del config.dict_tarefas[id_tarefa]
     salvar_arquivos(dict_tarefa=config.dict_tarefas)

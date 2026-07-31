3786233243f182e28f59d1876d498a90e41b54cb
Luiz Orlando <128615584+LuizOrla@users.noreply.github.com>
2026-07-31 13:04:37 -0300

    Update main.py
    Atualização com comentários do arquivo principal

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 # Arquivo principal de orquestração
+# Responsável por iniciar a aplicação
+
 from storage import ler_arquivos
 from gui import cabecalho
 from gui import menu_lateral
 from gui import corpo
 from nicegui import ui
-import config
 
-ler_arquivos()
+ler_arquivos() # Chama a função que lê os arquivos
 
-cabecalho("Projeto Teste")
-menu_lateral()
-corpo()
-ui.run()
+cabecalho("Gerenciamento de Tarefas") # Inicia a aplicação
+menu_lateral() # Renderiza o menu lateral
+corpo() # Renderiza o corpo da aplicação
+ui.run() # Roda a aplicação

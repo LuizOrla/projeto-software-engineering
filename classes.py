75dc1a1f95b4762390fce5d458ca13cb90c66781
Luiz Orlando <128615584+LuizOrla@users.noreply.github.com>
2026-07-31 16:15:20 -0300

    Update classes.py
    Atualização de código com comentários. Ajuste de código para acompanhar ajuste de funções

--- classes.py
+++ classes.py
@@ -1,45 +1,55 @@
-# Classes pertinentes à aplicação
+# Arquivo de classes principais de projeto
 
-# ========== Projetos ==========
-# Atributos
-#   ID
-#   Título
-#   Descrição
-#   Tarefas
-
-class Projeto:
+class Projeto: # Objeto Projeto, que cria dicionário relativo ao projeto para salvamento
     """
     Estrutura do dicionário:
     ```
     dict_projeto = {
-        "id_projeto": {
+        id_projeto: {
             "titulo": Nome do Projeto,
-            "descricao": Descrição do Projeto,
-            "tarefas": {
-                "id_tarefa": {
-                    "nome": Nome da Tarefa,
-                    "descricao_tarefa": Descrição da Tarefa,
-                    "status": Status da Tarfa
-                }
-            }
+            "descricao": Descrição do Projeto
         }
     }
     """
-    def __init__(self, id, titulo, descricao):
+    def __init__(self, id, titulo, descricao): # Função inicializadora, com titulo, descrição e ID
         self.id = id
         self.titulo = titulo
         self.descricao = descricao
-        self.tarefas = {}
 
-# ========== Tarefa ==========
-# Atributos
-#   ID
-#   Título
-#   Descrição
-#   Status
-class Tarefas:
-    def __init__(self, id, nome, descricao, status="todo"):
+    def dicionario_projeto(self): # Função que cria entrada em dicionário
+        id_dict = self.id
+        dict = {
+            "titulo": self.titulo,
+            "descricao": self.descricao
+        }
+        return id_dict, dict
+
+class Tarefa:
+    '''
+    Estrutura do dicionário:
+    ```
+    dict_tarefa = {
+        id_tarefa: {
+            "projeto": ID do projeto,
+            "nome": Nome da Tarefa,
+            "descricao": Descrição da Tarefa,
+            "status": Status da Tarefa
+        }
+    }
+    '''
+    def __init__(self, id, id_projeto, nome, descricao, status="todo"): # Função inicializadora, com titulo, descrição, ID, ID do projeto e status
         self.id = id
+        self.id_projeto = id_projeto
         self.nome = nome
         self.descricao = descricao
         self.status = status
+
+    def dicionario_tarefa(self): # Função que cria entrada em dicionário
+        id_dict = self.id
+        dict = {
+            "projeto": self.id_projeto,
+            "nome": self.nome,
+            "descricao": self.descricao,
+            "status": self.status
+        }
+        return id_dict, dict

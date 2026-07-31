1e67dbcc65acca85a151371d8befec9928a98888
Luiz Orlando <128615584+LuizOrla@users.noreply.github.com>
2026-07-31 16:11:58 -0300

    Update config.py
    Atualização de comentários de código

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 # Arquivo de constantes e configurações da aplicação
-import os
+import os # Importa pacote os, para acessar informações do Sistema Operacional
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__)) # Função que retorna o caminho para este arquivo em format "str"
 
-ARQUIVO_PROJETO = os.path.join(BASE_DIR, "data", "projetos.json")
+# Adiciona ao caminho coletado a finalização que aponta para os arquivos projetos.json e tarefas.json
+ARQUIVO_PROJETO = os.path.join(BASE_DIR, "data", "projetos.json") 
 ARQUIVO_TAREFAS = os.path.join(BASE_DIR, "data", "tarefas.json")
 
+# Dicionários onde são guardadas as informações retiradas dos arquivos
 dict_projetos = {}
 dict_tarefas = {}
 
+# Variável que guarda o ID do projeto ativo, que está sendo mostrado na tela
 projeto_ativo = ""
